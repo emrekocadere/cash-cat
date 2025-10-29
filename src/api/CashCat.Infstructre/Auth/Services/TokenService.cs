ce12cb96bafbc171f5f6daa7e81efc132c1ae479
emrekocadere <emrekocadere34@gmail.com>
2025-10-29 22:57:28 +0300

    feat: add JWT authentication and token service implementation

---
+++ src/api/CashCat.Infstructre/Auth/Services/TokenService.cs
@@ -0,0 +1,87 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using CashCat.Domain.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CashCat.Infstructre.Auth.Services;
+
+public class TokenService (IConfiguration configuration) : ITokenService
+{
+    public string GenerateAccessToken(IEnumerable<Claim> claims)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler(); // Token üretmk ve doğrulamak için kütphane.
+        
+        
+        // JWT oluştururken kullanılan gizli anahtarı (secret key) tanımlar ve SymmetricSecurityKey nesnesine çevirir.
+        var authSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(configuration["JWT:Secret"])); 
+
+        //JWT oluşturmak için gerekli tüm bilgileri içeren bir yapıdır.
+        var tokenDescriptor = new SecurityTokenDescriptor 
+        {
+            Issuer = configuration["JWT:ValidIssuer"],
+            Audience = configuration["JWT:ValidAudience"],
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddMinutes(15),
+            //Token'ın güvenliğini sağlamak için imza (signature) kısmını üretir
+            //Token’ın sahte olmadığını ispatlayan dijital imza bilgisi
+            SigningCredentials = new SigningCredentials
+                (authSigningKey, SecurityAlgorithms.HmacSha256)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor); 
+
+        return tokenHandler.WriteToken(token);
+    }
+    
+    public string GenerateRefreshToken()
+    {
+
+        var randomNumber = new byte[32];
+
+        using var randomNumberGenerator = RandomNumberGenerator.Create();
+        randomNumberGenerator.GetBytes(randomNumber);
+
+        
+        return Convert.ToBase64String(randomNumber);
+    }
+    
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string accessToken)
+    {
+        // Define the token validation parameters used to validate the token.
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWT:ValidAudience"],
+            ValidIssuer = configuration["JWT:ValidIssuer"],
+            ValidateLifetime = false,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        // Validate the token and extract the claims principal and the security token.
+        var principal = tokenHandler.ValidateToken(accessToken, tokenValidationParameters, out SecurityToken securityToken);
+
+        // Cast the security token to a JwtSecurityToken for further validation.
+
+        var jwtSecurityToken = securityToken as JwtSecurityToken;
+
+        // Ensure the token is a valid JWT and uses the HmacSha256 signing algorithm.
+        // If no throw new SecurityTokenException
+        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals
+                (SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Invalid token");
+        }
+
+        // return the principal
+        return principal;
+    }
+}
--- src/api/CashCat.Infstructre/Extensions/ServiceCollectionsExtensions.cs
+++ src/api/CashCat.Infstructre/Extensions/ServiceCollectionsExtensions.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using CashCat.Infstructre.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CashCat.Infstructre.Extensions;
 
@@ -9,11 +12,36 @@ public static class ServiceCollectionsExtensions
 {
     public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-         var connectionString = configuration.GetConnectionString("CashCatDbConnection");
-         
-         services.AddDbContext<CashCatDbContext>(opt=>
-         {
-             opt.UseNpgsql(connectionString);
-         });
+        
+        var connectionString = configuration.GetConnectionString("CashCatDbConnection");
+
+        services.AddDbContext<CashCatDbContext>(opt=>
+        {
+            opt.UseNpgsql(connectionString);
+        });
+
+        
+
+        services.AddAuthentication(options => // nden kütüphaneyi indridikten sonra çalıştı. sonuçta services başka yerde. program.cs te kabul ediyor
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options => // Uygulamaya diyor ki: "Ben gelen isteklerde Authorization: Bearer {token} başlığına bakacağım."
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+    
+                options.TokenValidationParameters = new TokenValidationParameters //Gelen token’ı TokenValidationParameters kurallarına göre kontrol edecek:
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                };
+            });
     }
 }
