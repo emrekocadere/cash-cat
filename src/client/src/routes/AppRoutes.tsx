89c6916c78b555b75c1cbe9d424198ebcb2b281b
emrekocadere <emrekocadere34@gmail.com>
2025-11-16 17:59:26 +0300

    feat: add main application routes component

---
+++ src/client/src/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import { RegisterPage } from '@/pages/RegisterPage';
+import { LoginPage } from '@/pages/LoginPage';
+
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+    </Routes>
+  );
+};
