7cfd1a8e0e8f02a1638af30b1090e37ec5b3caef
emrekocadere <emrekocadere34@gmail.com>
2025-12-11 17:21:41 +0300

    fix: correct controller name typo and set refresh token cookie

--- src/api/CashCat.API/Controllers/IdentityContoroller.cs
+++ src/api/CashCat.API/Controllers/IdentityContoroller.cs
@@ -10,19 +10,30 @@ namespace CashCat.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class IdentityContoroller(IMediator mediator) : ControllerBase
+public class IdentityController(IMediator mediator) : ControllerBase
 {
     [HttpPost("register")]
     public async Task<ActionResult<ResultT<TokenDto>>> Register(RegisterCommand command)
     {
-        var result= await mediator.Send(command);
+        var result = await mediator.Send(command);
         return result;
     }
-    
+
     [HttpPost("login")]
     public async Task<ActionResult<ResultT<TokenDto>>> Login(LoginCommand command)
     {
-        var result= await mediator.Send(command);
+
+        var result = await mediator.Send(command);
+        
+        Response.Cookies.Append("refreshToken", result.Value.RefreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = false,
+            SameSite = SameSiteMode.Lax,
+            Expires = DateTime.UtcNow.AddDays(7),
+            Path = "/"
+        });
+
         return result;
     }
 }
